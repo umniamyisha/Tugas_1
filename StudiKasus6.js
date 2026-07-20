c7f372cdcc601b9b3e83b36d449656e8321dcce9
umniamyisha <umniamyisha@gmail.com>
2026-07-20 15:42:35 +0700

    mayann

---
+++ StudiKasus6.js
@@ -0,0 +1,10 @@
+const umur = [8, 13, 17, 20, 25, 11];
+
+for (let i = 0; i < umur.length; i++) {
+    if (umur[i] >= 17) {
+        console.log("Pengunjung ke-" + (i + 1) + ":" + umur[i] + " Boleh Masuk");
+    } else {
+        console.log("Pengunjung ke-" + (i + 1) + ":" + umur[i] + "Tidak Boleh Masuk");
+
+    }
+}
