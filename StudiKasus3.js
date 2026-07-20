49b5077e53c0dc3f9cd1dd07a8675e9483d8a61d
umniamyisha <umniamyisha@gmail.com>
2026-07-20 15:05:20 +0700

    masii AMANN

---
+++ StudiKasus3.js
@@ -0,0 +1,16 @@
+const hero = [
+"Layla",
+"Ling",
+"Franco",
+"Fanny",
+"Miya",
+"Eudora"
+];
+
+for (let i = 0; i < hero.length; i++) {
+    if (hero[i].startsWith("F")) {
+        console.log(hero[i] + " -> Hero Assassin/Tank Favorit");
+    } else { 
+        console.log(hero[i] + " -> Hero Biasa");
+    }
+}
