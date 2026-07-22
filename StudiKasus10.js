55cdc7a76102dbaaf31b0edeb832e21e70aecc85
umniamyisha <umniamyisha@gmail.com>
2026-07-22 14:07:15 +0700

    selesai

--- StudiKasus10.js
+++ StudiKasus10.js
@@ -5,10 +5,8 @@ for (let i = 0; i < skor.length; i++) {
         console.log("Player " + (i + 1) + " : " + skor[i] + " Mythic");
     } else if (skor[i] >= 500) {
         console.log("Player " + (i + 1) + " : " + skor[i] + " Legend");
-
     } else if (skor[i] >= 300) {
          console.log("Player " + (i + 1) + " : " + skor[i] + " Epic");
-
     } else 
         console.log("Player " + (i + 1) + " : " + skor[i] + " Warrior");
 }
--- StudiKasus6.js
+++ StudiKasus6.js
@@ -2,7 +2,7 @@ const umur = [8, 13, 17, 20, 25, 11];
 
 for (let i = 0; i < umur.length; i++) {
     if (umur[i] >= 17) {
-        console.log("Pengunjung ke-" + (i + 1) + ":" + umur[i] + " Boleh Masuk");
+        console.log("Pengunjung ke-" + (i + 1) + ":" + umur[i] + "Boleh Masuk");
     } else {
         console.log("Pengunjung ke-" + (i + 1) + ":" + umur[i] + "Tidak Boleh Masuk");
 
